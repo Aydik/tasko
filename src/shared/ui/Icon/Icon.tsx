284c42ac15fe32ab4d7bbd6f51d2e48286f7e5a0
TheKeilMen <timofiei.tarasov@gmail.com>
2025-04-18 00:17:11 +0300

    fix

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -10,7 +10,7 @@ export const Icon: FC<Props> = ({name, size}) => {
         <svg xmlns="http://www.w3.org/2000/svg"
             style={{
                 width: `${size}`,
-                height: '${size}'
+                height: `${size}`
             }}
         >
             <use xlinkHref={`/sprite.svg#${name}`}></use>
