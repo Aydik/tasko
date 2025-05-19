7b2ecfe8de9684fd7f4790f2ffaadafd1981756e
TheKeilMen <timofiei.tarasov@gmail.com>
2025-05-19 17:53:11 +0300

    some work

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import { ReactElement } from 'react';
+import {FC, ReactElement} from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
 
 import './styles/index.module.scss';
 
-function App(): ReactElement {
+const App: FC = () => {
   return (
     <Router>
       <AppRouter />
--- src/shared/ui/Typography/index.tsx
+++ src/shared/ui/Typography/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import {FC, ReactNode} from 'react';
 import styles from './index.module.scss';
 
 interface Props {
