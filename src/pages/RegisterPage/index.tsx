4a60addd6d5a12db127ecac5c74c62f78a98f496
TheKeilMen <timofiei.tarasov@gmail.com>
2025-05-26 12:47:54 +0300

    added dnd-kit

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import styles from './index.module.scss';
-import { RegistrationForm } from 'features/registration/ui/RegistrationForm.tsx';
+import { RegistrationForm } from '../../features/registration/ui/RegistrationForm.tsx';
 
 export const RegisterPage: FC = () => {
   return <RegistrationForm />;
